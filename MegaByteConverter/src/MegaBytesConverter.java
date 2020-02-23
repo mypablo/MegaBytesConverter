634361c00bb4e57e9fae031f3fe982dbd903e77e
Yianni PM <51059714+mypablo@users.noreply.github.com>
2020-02-23 14:47:48 -0500

    Update MegaBytesConverter.java

--- MegaByteConverter/src/MegaBytesConverter.java
+++ MegaByteConverter/src/MegaBytesConverter.java
@@ -2,12 +2,15 @@ public class MegaBytesConverter {
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
 
-
+    //variables
         int megabytes;
         int kilobytes2;
+        
+    //calculating
         megabytes = kiloBytes / 1024;
         kilobytes2 = kiloBytes % 1024;
-
+        
+    //if-loop
         if (kiloBytes<0){
             System.out.println("Invalid Value");
         }else {
